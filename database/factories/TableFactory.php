4630c0cf3a104071c83e270286912ac92a92a351
JuanLuis <juanluislopezdaw@gmail.com>
2022-10-27 21:28:41 +0200

    Only C of CRUD && Get all

---
+++ database/factories/TableFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+        ];
+    }
+}
